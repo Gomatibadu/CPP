b515b7bcea3c20c4337e53ec27e665cc00124adb
Gomati Badu <badugomati886@gmail.com>
2025-03-11 18:43:28 +0530

    Add files via upload

---
+++ Template function overloading with normal function.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+template<class t>
+t sum(ta,tb)
+  
+{
+    t s=a+b;
+    return s;
+    
+}
+int sum (int a,intb)
+{
+    return(a+b);
+}
+int main()
+{
+    int a=5,b=7;
+    float=3.2,d=5.7;
+    cout<<"sum of a and b="<<sum(a,b);
+    cout<<"\n sum of c and d="<<sum(c,d)<<endl;
+    return 0;
+}
